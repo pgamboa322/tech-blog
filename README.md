aafd3a1f110a530a53c4704981bef112f3fa8c8c
pgamboa322 <pgamboa322@gmail.com>
2022-08-21 20:18:50 -0500

    fixed styling.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # tech-blog
+
+2 terminals 
+
+1 is mysql 
+login: mysql -u  root -p 
+enter pw: root
+
+then run source db/schema.sql
+
+switch to new term
+
+2 npm run seed
+node server.js
--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,3 @@
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: '127.0.0.1',
-//       dialect: 'mysql',
-//       port: 8080
-//     }
-//   );
-  
-
-// }
-
-// module.exports = sequelize;
-
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
